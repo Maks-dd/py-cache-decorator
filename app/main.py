6a35026189ee41c19b9f2506e841ab95dda1e5d6
Maksym Diadiushka <maks.d19@ukr.net>
2026-02-09 10:59:24 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from typing import Callable
 
 def cache(func: Callable) -> Callable:
     result = {}
-    def wrapper(*args, **kwargs):
+
+    def wrapper(*args, **kwargs) -> Callable:
         key = (args, tuple(kwargs.items()))
         if key not in result:
             result[key] = func(*args, **kwargs)
@@ -12,11 +13,3 @@ def cache(func: Callable) -> Callable:
             print("Getting from cache")
         return result[key]
     return wrapper
-
-@cache
-def long_time_func(a: int, b: int, c: int) -> int:
-    return (a ** b ** c) % (a * c)
-
-@cache
-def long_time_func_2(n_tuple: tuple, power: int) -> int:
-    return [number ** power for number in n_tuple]
